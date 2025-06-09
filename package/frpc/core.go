9203a5aa69a3a025a2dd59e5723fd66459075f4b
sian <2958422596@qq.com>
2025-06-09 11:34:25 +0800

    add opt:load configuration from string

--- package/frpc/core.go
+++ package/frpc/core.go
@@ -4,8 +4,33 @@ import (
 	"context"
 	"github.com/SianHH/frp-package/client"
 	v1 "github.com/SianHH/frp-package/pkg/config/v1"
+	"github.com/SianHH/frp-package/pkg/config/v1/validation"
+	"github.com/SianHH/frp-package/pkg/featuregate"
 	"time"
 )
+
+type Option func(s *Service) error
+
+func FromBytes(data []byte) Option {
+	return func(s *Service) error {
+		cfg, proxyCfgs, visitorCfgs, _, err := LoadClientConfig(data, true)
+		if err != nil {
+			return err
+		}
+		if len(cfg.FeatureGates) > 0 {
+			if err := featuregate.SetFromMap(cfg.FeatureGates); err != nil {
+				return err
+			}
+		}
+		if _, err := validation.ValidateAllClientConfig(cfg, proxyCfgs, visitorCfgs); err != nil {
+			return err
+		}
+		s.common = *cfg
+		s.proxyCfgs = proxyCfgs
+		s.visitorCfgs = visitorCfgs
+		return nil
+	}
+}
 
 type Service struct {
 	common      v1.ClientCommonConfig
@@ -15,13 +40,19 @@ type Service struct {
 	stopChan    chan struct{}
 }
 
-func NewService(common v1.ClientCommonConfig, proxyCfgs []v1.ProxyConfigurer, visitorCfgs []v1.VisitorConfigurer) *Service {
-	return &Service{
+func NewService(common v1.ClientCommonConfig, proxyCfgs []v1.ProxyConfigurer, visitorCfgs []v1.VisitorConfigurer, opts ...Option) (*Service, error) {
+	s := &Service{
 		common:      common,
 		proxyCfgs:   proxyCfgs,
 		visitorCfgs: visitorCfgs,
 		stopChan:    make(chan struct{}),
 	}
+	for _, opt := range opts {
+		if err := opt(s); err != nil {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 func (s *Service) Start() (err error) {
--- package/frps/core.go
+++ package/frps/core.go
@@ -6,17 +6,36 @@ import (
 	"github.com/SianHH/frp-package/server"
 )
 
+type Option func(s *Service) error
+
+func FromBytes(data []byte) Option {
+	return func(s *Service) error {
+		svrCfg, _, err := LoadServerConfig(data, true)
+		if err != nil {
+			return err
+		}
+		s.cfg = *svrCfg
+		return nil
+	}
+}
+
 type Service struct {
 	cfg      v1.ServerConfig
 	svc      *server.Service
 	stopChan chan struct{}
 }
 
-func NewService(cfg v1.ServerConfig) *Service {
-	return &Service{
+func NewService(cfg v1.ServerConfig, opts ...Option) (*Service, error) {
+	s := &Service{
 		cfg:      cfg,
 		stopChan: make(chan struct{}),
 	}
+	for _, opt := range opts {
+		if err := opt(s); err != nil {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 func (s *Service) Start() (err error) {
